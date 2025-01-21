04129b3d153c96d6d12410f51508ab1199142610
Lyssa Li <lyssa.li137@gmail.com>
2025-01-21 10:31:00 -0800

    layout samples

---
+++ src/assets/img.ts
@@ -0,0 +1,12 @@
+export const img: { [key: string] : string } = {
+  building: new URL('./building.webp', import.meta.url).href,
+  egg: new URL('./egg.webp', import.meta.url).href,
+  u1: new URL('./u1.webp', import.meta.url).href,
+  u2: new URL('./u2.webp', import.meta.url).href,
+  u3: new URL('./u3.webp', import.meta.url).href,
+  u4: new URL('./u4.webp', import.meta.url).href,
+  u5: new URL('./u5.webp', import.meta.url).href,
+  u6: new URL('./u6.webp', import.meta.url).href,
+  u7: new URL('./u7.webp', import.meta.url).href,
+  u8: new URL('./u8.webp', import.meta.url).href,
+};
