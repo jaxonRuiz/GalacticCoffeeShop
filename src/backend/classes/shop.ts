8ce74fd2e1ce308ea622482c34bb3edffdea6d7f
Jaxon Ruiz <45025385+jaxonRuiz@users.noreply.github.com>
2025-01-30 16:05:57 -0800

    roles rework, added actions

--- src/backend/classes/shop.ts
+++ src/backend/classes/shop.ts
@@ -7,6 +7,8 @@ export class Shop {
   w_emptyCups: Writable<number> = writable(0);
   w_coffeeCups: Writable<number> = writable(0); // sellable coffee
   w_waitingCustomers: Writable<number> = writable(0);
+  w_money: Writable<number> = writable(0);
+  w_appeal: Writable<number> = writable(0);
 
   // writable getters/setters
   get beans() {
@@ -33,33 +35,72 @@ export class Shop {
   set waitingCustomers(value) {
     this.w_waitingCustomers.set(value);
   }
+  get money() {
+    return get(this.w_money);
+  }
+  set money(value) {
+    this.w_money.set(value);
+  }
+  get appeal() {
+    return get(this.w_appeal);
+  }
+  set appeal(value) {
+    this.w_appeal.set(value);
+  }
 
+  // is an object instead of map because fixed entries
   restockSheet: { [key: string]: number } = {
     beans: 0,
     coffeeCups: 0,
   };
+  workerRates: { [key: string]: number } = {
+    baristaProductivity: 0.2,
+    serverProductivity: 0.5,
+  };
+  progressTrackers: { [key: string]: number } = {
+    serviceProgress: 0,
+    coffeeProgress: 0,
+    promoteProgress: 0,
+  };
+
+  coffeePrice: number = 5;
   roles: Map<string, Role> = new Map();
   totalWorkers: number = 0;
   freeWorkers: number = 0;
+  maxCustomers: number = 7;
+  promotionEffectiveness: number = 0.2;
+  minimumAppeal: number = 0.1;
+  maxAppeal: number = 2;
+
   constructor() {
+    // setting up default roles
     this.roles.set("barista", {
       name: "Barista",
       numWorkers: 0,
       maxWorkers: 1,
       wage: 50,
-      rates: {
-        coffeeProduction: 0.2,
-        beanConsumption: 0.5,
-      },
       update: (shop: Shop) => {
         let barista = shop.roles.get("barista")!;
-        if (
-          shop.beans > barista.rates["beanConsumption"] &&
-          shop.emptyCups > barista.rates["coffeeProduction"]
-        ) {
-          shop.beans -= barista.rates["beanConsumption"];
-          shop.emptyCups -= barista.rates["coffeeProduction"];
-          shop.coffeeCups += barista.rates["coffeeProduction"];
+        if (shop.beans >= 1 && shop.emptyCups >= 1) {
+          shop.progressTrackers["coffeeProgress"] +=
+            shop.workerRates["baristaProductivity"] * barista.numWorkers;
+        }
+      },
+    });
+
+    this.roles.set("server", {
+      name: "Server",
+      numWorkers: 0,
+      maxWorkers: 1,
+      wage: 50,
+      update: (shop: Shop) => {
+        let server = shop.roles.get("server")!;
+        if (shop.waitingCustomers > 0 && shop.coffeeCups > 0) {
+          shop.progressTrackers["serviceProgress"] +=
+            shop.workerRates["serverProductivity"] * server.numWorkers;
+        }
+        if (shop.progressTrackers["serviceProgress"] >= 1) {
+          shop.sellCoffee();
         }
       },
     });
@@ -70,14 +111,60 @@ export class Shop {
     this.roles.forEach((role: Role) => {
       role.update(this);
     });
+
+    // (should have used map)
+    if (this.progressTrackers["coffeeProgress"] >= 1) {
+      if (this.produceCoffee()) this.progressTrackers["coffeeProgress"] -= 1;
+    }
+    if (
+      this.progressTrackers["appealProgress"] >= 1 &&
+      this.waitingCustomers < this.maxCustomers
+    ) {
+      this.waitingCustomers++;
+      this.progressTrackers["appealProgress"] -= 1;
+    }
+    if (this.progressTrackers["serviceProgress"] >= 1) {
+      if (this.sellCoffee()) this.progressTrackers["serviceProgress"] -= 1;
+    }
   }
 
+  // actions
+  produceCoffee() {
+    if (this.beans >= 1 && this.emptyCups >= 1) {
+      this.beans--;
+      this.emptyCups--;
+      this.coffeeCups++;
+      return true;
+    }
+    return false;
+  }
+
+  sellCoffee() {
+    if (this.waitingCustomers >= 1) {
+      this.coffeeCups--;
+      this.waitingCustomers--;
+      this.money += this.coffeePrice;
+      return true;
+    }
+    return false;
+  }
+
+  promote() {
+
+  }
+
+  withdrawMoney() {
+    let money = this.money;
+    this.money = 0;
+    return money;
+  }
+
+  // TODO frame to unlock new jobs
   unlockSupplier() {
     this.roles.set("supplier", {
       name: "Supplier",
       numWorkers: 0,
       maxWorkers: 1,
-      rates: {},
       wage: 100,
       update: (owner: Shop) => {
         console.log("supplier updated");
@@ -91,6 +178,5 @@ interface Role {
   numWorkers: number;
   maxWorkers: number;
   wage: number; // weekly
-  rates: { [key: string]: number };
   update: (owner: Shop) => void;
 }
