744ddca171a9e86faf617d25e18d9d76935c200d
Lyssa Li <lyssa.li137@gmail.com>
2025-02-07 17:51:37 -0800

    keying i18n for shop

--- src/components/Shop.svelte
+++ src/components/Shop.svelte
@@ -1,49 +1,49 @@
 <script lang="ts">
   import Dropdown from "./Dropdown.svelte";
-
+  import { t } from "svelte-i18n";
 </script>
 
 <main class="shop container">
   <div class="shop left col">
     <div class="col">
       <h1>shop name</h1>
-      <p>money: ##</p>
-      <p>appeal: ##</p>
-      <p>sellable coffee: ##</p>
+      <p>{$t("money_stat")}: ##</p>
+      <p>{$t("appeal_stat")}: ##</p>
+      <p>{$t("sellableCoffee_stat")}: ##</p>
     </div>
   </div>
 
   <div class="shop right row">
     <div class="col">
       <div class="col block">
-        <Dropdown title="making coffee" classes={["col"]}>
-          <p>beans: ##</p>
-          <p>empty cups: ##</p>
-          <button>make coffee</button>
+        <Dropdown title={$t("making_title")} classes={["col"]}>
+          <p>{$t("beans_stat")}: ##</p>
+          <p>{$t("emptyCups_stat")}: ##</p>
+          <button>{$t("makeCoffee_btn")}</button>
         </Dropdown>
       </div>
       <div class="col block">
-        <Dropdown title="selling coffee" classes={["col"]}>
-          <p>appeal: ##</p>
-          <p>customers waiting: ##</p>
-          <button>promote coffee</button>
-          <button>sell coffee</button>
+        <Dropdown title={$t("selling_title")} classes={["col"]}>
+          <p>{$t("appeal_stat")}: ##</p>
+          <p>{$t("customersWaiting_stat")}: ##</p>
+          <button>{$t("promote_btn")}</button>
+          <button>{$t("sellCoffee_btn")}</button>
         </Dropdown>
       </div>
       <div class="col block">
-        <Dropdown title="cleaning shop" classes={["col"]}>
+        <Dropdown title={$t("cleaning_title")} classes={["col"]}>
           <p>smth</p>
         </Dropdown>
       </div>
       <div class="col block">
-        <Dropdown title="restocking" classes={["col"]}>
+        <Dropdown title={$t("restocking_title")} classes={["col"]}>
           <p>smth</p>
         </Dropdown>
       </div>
     </div>
     <div class="col">
       <div class="col block">
-        <h1>upgrades</h1>
+        <h1>{$t("upgrades_title")}</h1>
       </div>
     </div>
   </div>
