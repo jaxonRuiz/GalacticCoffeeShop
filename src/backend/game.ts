8f520631880dd65eca54b491a07ebd52630dbe2a
Jaxon Ruiz <45025385+jaxonRuiz@users.noreply.github.com>
2025-02-12 13:21:54 -0800

    update tabs

--- src/backend/game.ts
+++ src/backend/game.ts
@@ -5,7 +5,7 @@ import { Tester } from "./tester";
 import { Publisher } from "./systems/observer";
 import { MultiShop } from "./classes/multiShop";
 import { get, type Writable, writable } from "svelte/store";
-import { StageManager } from "./systems/stageManager";
+import { StageManager } from "./systems/sceneManager";
 
 // let tester = new Tester();
 // tester.preshopTest01();
@@ -17,14 +17,14 @@ console.log("hello world");
 startGame();
 
 function startGame() {
-  console.log("starting game");
-  sceneManager.nextScene();
+	console.log("starting game");
+	sceneManager.nextScene();
 }
 
 function saveState() {
-  // save state to local storage
+	// save state to local storage
 }
 
 function loadState() {
-  // load state from local storage
+	// load state from local storage
 }
