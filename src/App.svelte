e7c8c56fbc93ffebb766501993cb60b7678a43e0
Lyssa Li <lyssa.li137@gmail.com>
2025-02-12 11:04:55 -0800

    hide debug tabs from normal page

--- src/App.svelte
+++ src/App.svelte
@@ -7,16 +7,30 @@
 	let tabs = ["preshop", "shop", "multishop", "test"];
 	let comps = [Preshop, Shop, Multishop, Test];
 	let currentTab = $state(0);
+	let testing = $state(false); // open testing window
+
+	function on_key_down(event: KeyboardEvent) {
+		console.log(event.key);
+		if (event.key === "t") {
+			testing = !testing;
+		}
+	}
 </script>
 
+<svelte:window
+	onkeydown={on_key_down}
+/>
+
 <main class="fl">
-	<div id="tabs" class="row">
-		{#each tabs as tab, i}
-			<label class="tab">
-				<input type="radio" name="tab" value={i} bind:group={currentTab} />
-				<p>{tab}</p>
-			</label>
-		{/each}
+	<div id="test-window" style="display: {testing ? 'grid' : 'none'};">
+		<div id="tabs" class="col">
+			{#each tabs as tab, i}
+				<label class="tab">
+					<input type="radio" name="tab" value={i} bind:group={currentTab} />
+					<p>{tab}</p>
+				</label>
+			{/each}
+		</div>
 	</div>
 	<svelte:component this={comps[currentTab]} />
 </main>
@@ -28,21 +42,29 @@
 	}
 
 	/* temporary stuffs */
-	#tabs input {
-		display: none;
-	}
-	#tabs label {
-		cursor: pointer;
-		padding: 0.5em 4em;
-	}
 	label:has(input:checked) {
 		background-color: #242424;
 	}
-	#tabs {
-		background-color: #000000aa;
-		border: none;
-		justify-content: space-around;
-		width: 100%;
+	 
+	#test-window {
 		position: fixed;
+		background-color: #00000088;
+		z-index: 100;
+		width: 100%;
+		height: 100%;
+		display: grid;
+		#tabs {
+			place-self: center;
+			background-color: #000000;
+			border: none;
+			justify-content: space-around;
+			input {
+				display: none;
+			}
+			label {
+				cursor: pointer;
+				padding: 0.5em 4em;
+			}
+		}
 	}
 </style>
