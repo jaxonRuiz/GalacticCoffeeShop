4bdffe1623fed330fbdc5c3ab5c9574d557291a4
Lyssa Li <lyssa.li137@gmail.com>
2025-02-18 15:09:55 -0800

    restock sheet ui

--- src/frontend/pages/Shop.svelte
+++ src/frontend/pages/Shop.svelte
@@ -6,7 +6,7 @@
 	import { UpgradeManager } from "../../backend/systems/upgradeManager";
 	import Dropdown from "../components/Dropdown.svelte";
 	import { t } from "svelte-i18n";
-  import Worker from "../components/Worker.svelte";
+	import Worker from "../components/Worker.svelte";
 
 	// base
 	let timer = new Timer();
@@ -19,6 +19,7 @@
 	let umanager = new UpgradeManager("shop"); //TODO
 
 	// define variables
+	let mshopMoney = mshop.w_money;
 	let beans = sshop.w_beans;
 	let appeal = sshop.w_appeal;
 	let emptyCups = sshop.w_emptyCups;
@@ -27,13 +28,66 @@
 	let money = sshop.w_money;
 	let cleanness = sshop.w_cleanness;
 	let restockSheet = sshop.w_restockSheet;
+
+	function updateRestock(key: string, count: number) {
+		restockSheet.update((state) => {
+			return {
+				...state,
+				[key]: state[key] + count,
+			};
+		});
+	}
 </script>
+
+{#snippet restockItem(key: string)}
+	<div class="row restock">
+		<p>{$t(`${key}_text`)}</p>
+		<!-- <input type="number" min="0" step="1" value={`${$restockSheet.beans}`} /> -->
+		<button
+			hidden={true}
+			disabled={$restockSheet[key] >= 10 ? false : true}
+			onclick={() => {
+				updateRestock(key, -10);
+			}}>-10</button
+		>
+		<button
+			disabled={$restockSheet[key] >= 5 ? false : true}
+			onclick={() => {
+				updateRestock(key, -5);
+			}}>-5</button
+		>
+		<button
+			disabled={$restockSheet[key] >= 1 ? false : true}
+			onclick={() => {
+				updateRestock(key, -1);
+			}}>-1</button
+		>
+		<p>{$restockSheet[key]}</p>
+		<button
+			onclick={() => {
+				updateRestock(key, 1);
+			}}>+1</button
+		>
+		<button
+			onclick={() => {
+				updateRestock(key, 5);
+			}}>+5</button
+		>
+		<button
+			hidden={true}
+			onclick={() => {
+				updateRestock(key, 10);
+			}}>+10</button
+		>
+	</div>
+{/snippet}
 
 <main class="shop container">
 	<div class="shop left col">
 		<div class="col">
-			<h1>shop name</h1>
+			<h1>Coffee Shop</h1>
 			<p>{$t("money_stat")}: ${$money.toFixed(2)}</p>
+			<p>{$t("mshopMoney_stat")}: ${$mshopMoney.toFixed(2)}</p>
 			<p>{$t("appeal_stat")}: {(100 * $appeal).toFixed(2)}%</p>
 			<p>{$t("sellableCoffee_stat")}: {Math.floor($coffee)}</p>
 		</div>
@@ -76,8 +130,16 @@
 			</Dropdown>
 
 			<Dropdown title={$t("restocking_title")}>
-				<!-- TODO -->
+				<p>{$t("restockSheet_text")}</p>
+				{#each Object.keys($restockSheet) as key}
+					{@render restockItem(key)}
+				{/each}
 				<button
+					disabled={$restockSheet.beans * sshop.beansPrice +
+						$restockSheet.emptyCups * sshop.cupsPrice >
+					$mshopMoney + $money
+						? true
+						: false}
 					onclick={() => {
 						sshop.restock();
 					}}>{$t("restock_btn")}</button
@@ -96,5 +158,13 @@
 	.shop.right > div {
 		width: 50%;
 		overflow-y: scroll;
+	}
+
+	.restock {
+		align-items: center;
+		button {
+			margin-left: 0;
+			margin-right: 0;
+		}
 	}
 </style>
