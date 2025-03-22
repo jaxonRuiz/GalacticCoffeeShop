1ee03787a86ee2633b1b4b976099e2af1426ba2f
Lyssa <lyssa.li137@gmail.com>
2025-03-22 02:24:00 -0700

    forgot to add i18n... oops

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,15 +1,24 @@
-import { addMessages, init, getLocaleFromNavigator } from 'svelte-i18n';
+import { addMessages, init, getLocaleFromNavigator, locale } from 'svelte-i18n';
 
 // import translation packs 
 import en from './en.json';
-// import zh from './zh.json';
+import zh from './zh.json';
 
 // add translations to app
 addMessages('en', en);
-// addMessages('zh', zh);
+addMessages('zh', zh);
 
 // initialize i18n and language
 init({
 	fallbackLocale: 'en',
 	initialLocale: getLocaleFromNavigator(),
 });
+
+const locName = {
+	en: 'English',
+	zh: '简体中文',
+};
+
+export function setLanguage(loc: string) {
+	locale.set(loc);
+}
