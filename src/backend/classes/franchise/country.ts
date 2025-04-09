345043748fe383f06a6fe8e45fea3d281de4c8ea
axhiggin <93854415+AidanHig@users.noreply.github.com>
2025-04-08 20:10:31 -0700

    VERY INCOMPLETE, started a bit on the baseclass and city class which extends it, probably do a bit in region next to define the variables which InitializeDevelopment will have to reference

---
+++ src/backend/classes/franchise/country.ts
@@ -0,0 +1,28 @@
+import { Publisher } from "../../systems/observer";
+import { get, type Writable, writable } from "svelte/store";
+import { type LocalShopSave, Shop } from "../shop";
+import { UpgradeManager } from "../../systems/upgradeManager";
+import { cleanupAudioManagers, AudioManager } from "../../systems/audioManager";
+import { aud } from "../../../assets/aud";
+
+export class Country implements ISubscriber{
+    
+
+
+    notify(event: string, data?: any) {
+		// maybe optimize better :/ dont need to call every shop every tick
+		if (event === "tick") {
+			this.tick();
+		}
+		if (event === "day") {
+			
+		}
+		if (event === "week") {
+			
+		}
+	}
+
+    tick(){
+
+    }
+}
---
+++ src/backend/classes/franchise/developments/city.ts
@@ -0,0 +1,36 @@
+import { Publisher } from "../../../systems/observer";
+import { get, type Writable, writable } from "svelte/store";
+import { type LocalShopSave, Shop } from "../../shop";
+import { UpgradeManager } from "../../../systems/upgradeManager";
+import { cleanupAudioManagers, AudioManager } from "../../../systems/audioManager";
+import { aud } from "../../../../assets/aud";
+import { DevelopmentBase, DevelopmentType } from "./developmentbase";
+import type { Region } from "../region";
+
+export class City extends DevelopmentBase{
+    get developmentType(): DevelopmentType {
+        return DevelopmentType.City;
+      }
+
+    notify(event: string, data?: any) {
+		// maybe optimize better :/ dont need to call every shop every tick
+		if (event === "tick") {
+			this.tick();
+		}
+		if (event === "day") {
+			
+		}
+		if (event === "week") {
+			
+		}
+	}
+
+    tick(){
+
+    }
+
+    InitializeDevelopment(): void {
+        //put all the city specific initializations in here; much will be procedurally generated based on parent region's environment/allocated area size
+        
+    }
+}
