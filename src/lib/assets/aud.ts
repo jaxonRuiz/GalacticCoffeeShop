51e46f371114a5f00d25f971e0eff960643ec932
Marco J Ogaz-Vega <marcoogazvega@hotmail.com>
2025-04-16 14:05:00 -0700

    Added songs and pathing

--- src/lib/assets/aud.ts
+++ src/lib/assets/aud.ts
@@ -1,6 +1,8 @@
 export const aud: { [key: string]: string } = {
     shop_music: new URL('./music/Duraznito - Quincas Moreira.mp3', import.meta.url).href,
     preshop_music: new URL('./music/Squirm Worm - TrackTribe.mp3', import.meta.url).href,
+    cant_burn: new URL(`./music/Mid-Air Machine - Just Can't Burn.mp3`, import.meta.url).href,
+    moonlight: new URL('./music/snoozy beats - moonlight city.mp3', import.meta.url).href,
     boiling: new URL('./sfx/boiling.flac', import.meta.url).href,
     ding: new URL('./sfx/ding.wav', import.meta.url).href,
     cashRegister: new URL('./sfx/cashRegister.wav', import.meta.url).href,
