bcd7a613635ad077cbf2c12c9f557ef568cc7c23
maozblan <lyssa.li137@gmail.com>
2025-04-23 23:37:00 -0700

    coffees spawn and disappear on make and sell

---
+++ src/lib/backend/interface/uimanager.ts
@@ -0,0 +1,74 @@
+import { get, type Writable, writable } from "svelte/store";
+
+export class UIManager {
+	coffeeGenerator: CoffeeGenerator;
+
+	get coffees() {
+		return this.coffeeGenerator.coffees;
+	}
+
+	constructor() {
+		this.coffeeGenerator = new CoffeeGenerator([]);
+	}
+
+	coffeeMade(totalCoffees: number) {
+		const currCoffee = this.coffeeGenerator.coffeeCount;
+		for (let i = 0; i < Math.floor(totalCoffees - currCoffee); ++i) {
+			this.coffeeGenerator.addCoffee();
+		}
+	}
+
+	coffeeSold() {
+		// remove customer
+		this.coffeeGenerator.removeCoffee();
+	}
+}
+
+class CoffeeGenerator {
+	loc: number[][];
+	coffees: Writable<number[][]>;
+	overflow: number = 0;
+	
+	get coffeeCount () {
+		return get(this.coffees).length;
+	}
+
+	constructor(allLoc: number[][]) {
+		this.loc = allLoc;
+		this.coffees = writable([]);
+	}
+
+	updateLocations(loc: number[][]) {
+		this.loc = loc;
+		this.coffees.set([]);
+	}
+
+	addCoffee() {
+		if (this.coffeeCount == this.loc.length) {
+			this.overflow++;
+			return;
+		}
+		const possLoc = this.loc.filter((loc) => {
+			return !get(this.coffees).includes(loc);
+		});
+		this.coffees.update((coffees) => {
+			const newCoffee = possLoc[Math.floor(Math.random() * possLoc.length)];
+			coffees.push(newCoffee);
+			return coffees;
+		});
+	}
+
+	removeCoffee() {
+		this.coffees.update((coffees) => {
+			const newCoffees = [...coffees];
+			newCoffees.shift();
+			return newCoffees;
+		});
+		if (this.overflow > 0) {
+			this.overflow--;
+			setTimeout(() => {
+				this.addCoffee();
+			}, 200);
+		}
+	}
+}
