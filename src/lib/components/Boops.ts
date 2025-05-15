6de201a2bf723df4756dd372378c93554f0730b0
maozblan <lyssa.li137@gmail.com>
2025-05-15 01:19:05 -0700

    console log cleaning

--- src/lib/components/Boops.ts
+++ src/lib/components/Boops.ts
@@ -6,7 +6,6 @@ const maxBoopDuration = 1000;
 export function booped(x: number, y: number, type: string, num?: string) {
 	num = num || "";
 	boops.set([...get(boops), { x, y, type, id: Date.now() + Math.random(), num }]);
-	console.log(get(boops), get(boops).length);
 
 	setTimeout(() => {
 		boops.update((boops) => {
