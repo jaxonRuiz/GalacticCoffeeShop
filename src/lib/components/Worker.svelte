6e1b011a8913f8d02142cd632f14b87550bd0780
maozblan <lyssa.li137@gmail.com>
2025-05-24 20:26:11 -0700

    removing worker hire/fire in frontend

--- src/lib/components/Worker.svelte
+++ src/lib/components/Worker.svelte
@@ -9,22 +9,6 @@
 
 <div class="row worker {worker}">
 	<p>
-		{$t(`${worker}_worker`)}: {`${$WA[worker + "Current"]} / ${$WA[worker + "Max"]}`}
+		{$t(`${worker}_worker`)}: {`${$WA[worker + "Current"]}`}
 	</p>
-	<Button
-		move={false}
-		data-btn="plusOne"
-		disabled={$WA[worker + "Current"] == $WA[worker + "Max"]}
-		onclick={() => {
-			sshop.addWorker(worker);
-		}}>{$t("hireWorker_btn")}</Button
-	>
-	<Button
-		move={false}
-		data-btn="minusOne"
-		disabled={$WA[worker + "Current"] == 0}
-		onclick={() => {
-			sshop.removeWorker(worker);
-		}}>{$t("fireWorker_btn")}</Button
-	>
 </div>
